a9791a1b6c04d4aaa8767e4deb3841a2f9b27919
double <21sunrin077@sunrint.hs.kr>
2022-08-12 17:26:23 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const hanspell = require('hanspell');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use('views', express.static(path.join(__dirname, 'views')));
+
+app.use(express.urlencoded({ extended: false }));
+
+let sentence;
+let checkingSentence;
+
+app.route('/')
+  .get((req, res) => {
+    res.render('index', {sentence, checkingSentence});
+  })
+  .post(async (req, res) => {
+    sentence = req.body.sentence;
+    spellCheck(sentence);
+    console.log(checkingSentence);
+    res.render('index', {sentence, checkingSentence});
+  })
+
+
+app.listen(8000, () => {
+  console.log('server start');
+});
+
+function spellCheck(spell) {
+  const data = function (v) {
+    checkingSentence = v;
+  }
+  const end = function () {
+    console.log('// check ends');
+  };
+  const error = function (err) {
+    console.error('// error: ' + err);
+  };
+
+  hanspell.spellCheckByPNU(spell, 6000, data, end, error);
+
+}
